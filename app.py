27f33249db746282a94b6e671caa0ec984595b6e
almahyra-japan-center <arsipalmahyra@gmail.com>
2026-09-02 10:01:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.set_page_config(page_title="ALMAHYRA JAPAN CENTER", layout="wide")
 st.title("🇯🇵 ALMAHYRA JAPAN CENTER")
-st.write("Selamat datang di Sistem Informasi AMAHYRA J.C.")
-st.success("Web berhasil jalan! Ini versi awal")
+st.write("Selamat datang di lembaga bahasa jepang ALMAHYRA JAPAN CENTER🇯🇵")
+st.success("yuk belajar bahasa jepang dari dasar bersama kami ! Bergabunglah jadi keluarga besar dan wujudkan mimpimu")
 
 menu = st.sidebar.selectbox("Menu", ["Dashboard", "Data Siswa", "Keuangan"])
 if menu == "Dashboard":
